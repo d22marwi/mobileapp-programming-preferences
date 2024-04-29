a36a07b17bedb4a349fc6e5cbc4186ebd0ae94f4
d22marwi <d22marwi@student.his.se>
2024-04-29 17:19:17 +0200

    implementerade Shared Preferences.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.content.SharedPreferences;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,12 +13,21 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        myPreferenceRef = getSharedPreferences("key", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        TextView prefTextRef= findViewById(R.id.my_textview);
+        prefTextRef.setText(myPreferenceRef.getString("key", "No preference found."));
 
         Button ToSecondActivity = findViewById(R.id.my_button1);
         ToSecondActivity.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +37,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TextView sharedPrefData = findViewById(R.id.my_textview);
+        sharedPrefData.setText(myPreferenceRef.getString("key", "Name"));
     }
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -6,21 +6,36 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.content.SharedPreferences;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        myPreferenceRef = getSharedPreferences("key", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        EditText prefTextRef = findViewById(R.id.my_edittext);
+        prefTextRef.setText(myPreferenceRef.getString("key", "No preference found."));
+
         Button Submit = findViewById(R.id.my_button2);
         Submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                store();
                 Intent intent = new Intent(SecondActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
+    }
+    void store() {
+        EditText sharedPrefData = findViewById(R.id.my_edittext);
+        myPreferenceEditor.putString("key", sharedPrefData.getText().toString());
+        myPreferenceEditor.apply();
     }
 }
